849cd8b451df153ce2d086e44eabf5982fe32320
Simone Rizza <simonerizza@mbp-de-simone.home>
2022-11-30 17:11:09 +0100

    first commit

--- client/README.md
+++ client/README.md
@@ -1,4 +1,4 @@
-# chatapp
+# client
 
 ## Project setup
 ```
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	mongoUri: process.env.MONGO_URI,
+	PORT: process.env.PORT || 3000,
+};
+
+// Run the below command for each new terminal session
+// export MONGO_URI="mongodb+srv://simone:Nodejs2022@portfolio-cluster.z0ixmu0.mongodb.net/portfolio_data"
